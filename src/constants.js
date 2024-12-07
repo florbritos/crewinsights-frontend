4eae1a0e6829bad8b864287d5bbb05b302ad000d
Florencia Britos <flor-britos@hotmail.com.ar>
2024-12-07 12:43:02 -0300

    preparing deployment

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const LOCALHOSTURL = 'http://192.168.0.11:8000'
-export const PRODSERVERURL = 'https://web-production-b760e.up.railway.app/'
+export const PRODSERVERURL = 'https://web-production-b760e.up.railway.app'
 export const ACTIVEURL = PRODSERVERURL
